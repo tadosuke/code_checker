9f67ed1c7743e6c39fd129885aef34394dbf52af
tadosuke <tadosuke@gmail.com>
2023-09-17 15:00:23 +0900

    他プロジェクトからファイルをコピー

---
+++ check_annotation.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+import ast
+import os
+import subprocess
+import sys
+
+
+class TypeHintChecker:
+    """型ヒントをチェックするクラス."""
+
+    def check_type_hints_in_file(self, file_path: str) -> bool:
+        """Pythonファイル内の型ヒントをチェックする."""
+        tree = self._create_ast_tree(file_path)
+        issues: list[str] = []
+        for node in ast.walk(tree):
+            if not isinstance(node, (ast.FunctionDef, ast.AsyncFunctionDef)):
+                continue
+            self._check_argument_type_hints(node, issues)
+            self._check_return_type_hints(node, issues)
+
+        if issues:
+            for issue in issues:
+                sys.stderr.write(issue)
+            return False
+
+        return True
+
+    def _create_ast_tree(self, file_path: str) -> ast.AST:
+        """抽象構文木（AST）を生成する."""
+        with open(file_path, 'r', encoding='utf-8') as f:
+            tree = ast.parse(f.read())
+        return tree
+
+    def _check_argument_type_hints(self, node: ast.FunctionDef, issues: list[str]) -> None:
+        """引数の型ヒントをチェックする."""
+        args = node.args
+        for arg in args.args:
+            if self._is_python_reserved_arg(arg.arg):
+                continue
+            if arg.annotation is None:
+                issues.append(f"関数 '{node.name}' の引数 '{arg.arg}' に型ヒントがありません。\n")
+
+    def _is_python_reserved_arg(self, arg_name: str) -> bool:
+        """引数名がPythonの予約語かどうかチェックする."""
+        return arg_name == 'self' or arg_name == 'cls'
+
+    def _check_return_type_hints(self, node: ast.FunctionDef, issues: list[str]) -> None:
+        """戻り値の型ヒントをチェックする."""
+        returns = node.returns
+        if returns is None:
+            issues.append(f"関数 '{node.name}' の戻り値に型ヒントがありません。\n")
+
+
+class FileFinder:
+    """ファイルを探すクラス."""
+
+    @staticmethod
+    def get_files_from_git_diff() -> list[str]:
+        """Gitのdiffからファイル名を取得する."""
+        result = subprocess.run(["git", "diff", "--name-only", "HEAD", "HEAD~"], capture_output=True, text=True)
+        return result.stdout.strip().split("\n")
+
+    @staticmethod
+    def get_files_from_argv() -> list[str]:
+        """コマンドライン引数からファイル名を取得する."""
+        return sys.argv[1:]
+
+
+class FileFilter:
+    """ファイルをフィルタリングするクラス."""
+
+    @staticmethod
+    def is_test_file(file_name: str) -> bool:
+        """テスト用のファイルかどうかチェックする."""
+        return file_name.startswith('test_')
+
+    @staticmethod
+    def is_python_file(file_name: str) -> bool:
+        """Pythonファイルかどうかチェックする."""
+        return file_name.endswith('.py')
+
+
+def main() -> None:
+    """メイン関数."""
+    target_files = FileFinder.get_files_from_argv()
+    if not target_files:
+        sys.stderr.write('対象ファイルが指定されていません')
+        exit(1)
+
+    root_folder = os.getcwd()
+    type_checker = TypeHintChecker()
+    for file in target_files:
+        if not FileFilter.is_python_file(file):
+            continue
+        if FileFilter.is_test_file(file):
+            continue
+
+        full_file_path = os.path.join(root_folder, file)
+        if not type_checker.check_type_hints_in_file(full_file_path):
+            exit(1)
+
+
+if __name__ == "__main__":
+    main()
---
+++ check_docstring.py
@@ -0,0 +1,90 @@
+"""docstring と関数の引数・戻り値が一致しない場合に警告してくれるスクリプト."""
+
+import ast
+import re
+import sys
+
+
+class DocstringChecker:
+    """Docstring のチェックを行うクラス。
+
+    :param filename: チェックを行いたい Python ファイル名
+    """
+
+    def __init__(self, filename: str) -> None:
+        self.tree = self._parse_tree_from_file(filename)
+
+    def _parse_tree_from_file(self, filename: str) -> ast.Module:
+        """ファイルからASTを生成。"""
+        with open(filename, "r", encoding='utf-8') as f:
+            return ast.parse(f.read())
+
+    def _check_class_docstrings(self, node: ast.ClassDef) -> None:
+        """クラスのdocstringと__init__関数の引数をチェック。"""
+        docstring = ast.get_docstring(node)
+        if not docstring:
+            return
+        init_method = next((n for n in node.body if isinstance(n, ast.FunctionDef) and n.name == '__init__'), None)
+        if not init_method:
+            return
+        init_params = self._get_function_signature(init_method)
+        doc_params = self._parse_docstring(docstring)
+        for param in init_params.keys():
+            if param in ('return', 'self', 'cls'):
+                continue
+            if param not in doc_params:
+                print(f"警告: {node.name} クラスの __init__ で {param} がdocstringにありません。({init_method.lineno} 行目)")
+
+    def _check_function_docstrings(self, node: ast.FunctionDef) -> None:
+        """関数のdocstringと引数をチェック。"""
+        if any(isinstance(deco, ast.Name) and deco.id == 'property' for deco in node.decorator_list):
+            return
+        docstring = ast.get_docstring(node)
+        if not docstring:
+            return
+        doc_params = self._parse_docstring(docstring)
+        func_params = self._get_function_signature(node)
+        for param in func_params.keys():
+            if param not in doc_params and param not in ('self', 'cls'):
+                print(f"警告: {node.name} 関数で {param} がdocstringにありません。({node.lineno} 行目)")
+
+    def _parse_docstring(self, docstring: str) -> dict[str, str]:
+        """docstringからパラメーターを抽出。"""
+        lines = docstring.split("\n")
+        params = {}
+        for line in lines:
+            match = re.search(r":param (\w+):", line)
+            if match:
+                param_name = match.group(1)
+                params[param_name] = "param"
+            match = re.search(r":return:", line)
+            if match:
+                params["return"] = "return"
+        return params
+
+    def _get_function_signature(self, node: ast.FunctionDef) -> dict[str, str]:
+        """関数のシグネチャを取得。"""
+        signature = {}
+        for arg in node.args.args:
+            signature[arg.arg] = "param"
+        if node.returns:
+            signature["return"] = "return"
+        return signature
+
+    def check(self) -> None:
+        """メインのチェック関数。"""
+        for node in ast.walk(self.tree):
+            if isinstance(node, ast.ClassDef):
+                self._check_class_docstrings(node)
+            elif isinstance(node, ast.FunctionDef) or isinstance(node, ast.AsyncFunctionDef):
+                self._check_function_docstrings(node)
+
+
+def main():
+    filename = sys.argv[1]
+    checker = DocstringChecker(filename)
+    checker.check()
+
+
+if __name__ == "__main__":
+    main()
